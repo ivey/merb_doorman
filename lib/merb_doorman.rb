bf5f6309c001623a97c5ecd5879eb234426f6921
Michael D. Ivey <ivey@gweezlebur.com>
2008-06-18 19:40:12 -0500

    basically working, but untested

--- lib/merb_doorman.rb
+++ lib/merb_doorman.rb
@@ -1,18 +1,112 @@
-# make sure we're running inside Merb
-if defined?(Merb::Plugins)
+module Merb
+  module Plugins
+    module Doorman
+      
+      def included(base)
+        base.class_eval do
+          before :_doorman_check_acl
+        end
+      end
+      
+      
+      def _doorman_list
+        @_doorman_list ||= [:all]
+      end
+      
+      def _doorman_default
+        @_doorman_default ||= :allow
+      end
 
-  # Merb gives you a Merb::Plugins.config hash...feel free to put your stuff in your piece of it
+      def deny(*args,&block)
+        _add_acl(:deny,args,block)
+      end
+
+      def allow(*args,&block)
+        _add_acl(:allow,args,block)
+      end
+      
+      protected
+      def _add_acl(type,args,block)
+        if args == :all
+          _doorman_default = type
+          return true
+        end
+        if _valid_acl(args)
+          _doorman_list << [type,args]
+        elsif block && block.arity == 1
+          _doorman_list << [type,block]
+        else
+          raise "merb_doorman: invalid ACL entry #{args.inspect}"
+        end
+      end
+      
+      def _valid_acl(args)
+        args = args.is_a?(Array) ? args.first : args
+        if args.is_a?(Symbol)
+          return true
+        elsif args.is_a?(Hash)
+          return false if args.size > 1
+          return false unless args.keys.first.in? [:host, :user, :user_agent, :time]
+          return true
+        end
+      end
+      
+      def _check_acl(acl)
+        type,command = acl
+        match = if command.is_a?(Proc)
+          command.call(self)
+        elsif command.is_a?(Hash)
+          _check_acl_hash(command)
+        end
+        if type == :deny
+          return !match
+        else
+          return match
+        end
+      end
+      
+      def _doorman_check_acl
+        allowed = false
+        _doorman_list.each do |acl|
+          if _check_acl(acl)
+            allowed = true
+          else
+            throw :halt, "ACL failed: #{acl}"
+          end
+        end
+        if _doorman_default == :deny && !allowed
+          throw :halt, "ACL failed: deny :all"
+        end
+      end
+      
+      def _check_acl_hash(hash)
+        rule = hash.keys.first
+        value = hash.values.first
+        case rule.to_s
+        when :host
+          request.host =~ Regexp.new(value)
+        when :user
+          _doorman_user_block.call(self)
+        when :time
+          false #not implemented
+        when :user_agent 
+          request.user_agent =~ Regexp.new(value)
+        else
+          false
+        end
+      end
+      
+      def _doorman_user_block
+        Merb::Plugins.config[:merb_doorman][:current_user_block]
+      end
+    end
+  end
+end
+
+
+if defined?(Merb::Plugins)
   Merb::Plugins.config[:merb_doorman] = {
-    :chickens => false
+    :current_user_block => proc {|c| c.current_user.login }
   }
-  
-  Merb::BootLoader.before_app_loads do
-    # require code that must be loaded before the application
-  end
-  
-  Merb::BootLoader.after_app_loads do
-    # code that can be required after the application loads
-  end
-  
-  Merb::Plugins.add_rakefiles "merb_doorman/merbtasks"
+  Merb::Controller.send(:include, Merb::Plugins::Doorman)
 end
--- spec/merb_doorman_spec.rb
+++ spec/merb_doorman_spec.rb
@@ -1,7 +1,67 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
-describe "merb_doorman" do
-  it "should do nothing" do
-    true.should == true
+describe "merb_doorman DSL" do
+  include Merb::Plugins::Doorman
+
+  before(:each) do
+    @_doorman_list = nil
+  end
+
+  it "should store the ACL" do
+    _doorman_list.should be_an_instance_of(Array)
+  end
+  
+  it "should have an implicit allow :all" do
+    _doorman_list.should have(1).element
+    _doorman_list.first.should eql(:all)
+  end
+  
+  it "should have a deny method" do
+    lambda { deny :all }.should_not raise_error
+  end
+  
+  it "should store a deny entry on the ACL" do
+    s = _doorman_list.size
+    deny :host => "192.168.*"
+    _doorman_list.should have(s + 1).elements
+  end
+  
+  it "should have an allow method" do
+    lambda { allow :all }.should_not raise_error
+  end
+  
+  it "should store an allow entry on the ACL" do
+    s = _doorman_list.size
+    allow :user_agent => /MSIE/
+    _doorman_list.should have(s + 1).elements
+  end
+  
+  it "should allow valid ACL entries" do
+    lambda { deny :host => "192.168.*" }.should_not raise_error
+    lambda { deny :user => "bill" }.should_not raise_error
+    lambda { deny :user_agent => /MSIE/ }.should_not raise_error
+    lambda { deny :time => "8am-5pm" }.should_not raise_error
+    lambda { deny {|c| c.foo } }.should_not raise_error
+  end
+  
+  it "should reject invalid ACL entries" do
+    lambda { deny :foo => "3" }.should raise_error
+    lambda { deny { foo } }.should raise_error
   end
 end
+
+# deny :host => "209.34.*"
+# deny :user => "bill"       # calls current_user.login, but this is configurable
+# deny :user_agent => /MSIE/
+# deny {|c| c.params["arbitrary"] == "expressions"}
+# 
+# 
+# # mostly closed:
+# deny :all                           # removes implicit final allow :all
+# allow :host => "*.example.com"
+# allow :time => "8am-5pm"
+# 
+# # store a block for repeated usage
+# Merb::Access.add_block :admin, {|c| c.current_user.admin?}
+# 
+# allow :admin
